c9b8ef2b008db0ee5c4927b0ac33e9689d044d20
KOT BOTS <88032123+kotfreede@users.noreply.github.com>
2021-11-07 21:21:06 +0530

    Update messages.py

--- bot/messages.py
+++ bot/messages.py
@@ -4,11 +4,11 @@
 '''Bot Messages defined'''
 
 
-common_text = "\n\n<b><u>If you are facing any problem, so report at @AJPyroVerseGroup</u></b>"
+common_text = "\n\n<b><u>If you are facing any problem, so report at @KOT_REPORS</u></b>"
 
 help_msg = f"<b>To use me, Just Send me any raw link which contain video, and I will upload it to Pdisk.</b>{common_text}"
 
-start_msg = f"<b>Hi, I am Pdisk_Uploader_Bot Created by @AJPyroVerse and My Developer is @AJTimePyro.</b>\n{help_msg}"
+start_msg = f"<b>Hi, I am Pdisk_Uploader_Bot Created by @KOT_BOTS and My Developer is @KOT_FREE_DE_LA_HOYA_OFF.</b>\n{help_msg}"
 
 not_joined_community = f"<b>To use this bot, you need to Join our Channel and Group.</b>{common_text}"
 
@@ -18,4 +18,4 @@ unsuccessful_upload = f"Uploading went <b>unsuccessful</b>, maybe url has proble
 
 invalid_format_msg = f"<b>Send url with filename, like this <code>https://www.google.com Google.mp4</code>\n\nNote that filename should be with it's extension like <code>Spiderman.mp4</code></b>{common_text}"
 
-media_msg = f"<b>Send this file to @File2linkpyrobot, It will give you the link, then send that link to me.{common_text}</b>"
+media_msg = f"<b>Send this file to @KOT_FILE_TO_LINK_BOT, It will give you the link, then send that link to me.{common_text}</b>"
